addada62d5242aee680ecac6103e933c64ba5568
mckinleysouder <msouder@ucsd.edu>
2021-08-26 14:47:50 -0700

    dataset path changed

--- tf2_model.py
+++ tf2_model.py
@@ -139,8 +139,8 @@ class CycleGAN(object):
     def train(self, args):
 
         # Data from domain A and B, and mixed dataset for partial and full models.
-        dataA = glob('./datasets/{}/train/*.*'.format(self.dataset_A_dir))
-        dataB = glob('./datasets/{}/train/*.*'.format(self.dataset_B_dir))
+        dataA = glob('./Dataset/{}/train/*.*'.format(self.dataset_A_dir))
+        dataB = glob('./Dataset/{}/train/*.*'.format(self.dataset_B_dir))
         data_mixed = None
         if self.model == 'partial':
             data_mixed = dataA + dataB
